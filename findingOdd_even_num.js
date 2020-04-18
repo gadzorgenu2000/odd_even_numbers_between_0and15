64440d9cc5c9f1455f50c41a8a30eb7b37b0fbd3
gadzorgenu2000 <42786156+gadzorgenu2000@users.noreply.github.com>
2020-04-18 19:55:49 +0100

    first commit

---
+++ findingOdd_even_num.js
@@ -0,0 +1,9 @@
+for(let number =0; number<=15;number++){
+    if(number===0){
+        console.log(number+ " is even");
+    }else if(number%2 == 0){
+        console.log(number+ " is even");
+    }else{
+        console.log(number+ " is odd")
+    }
+}
